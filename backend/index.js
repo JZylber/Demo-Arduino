3a91381634dd91948753c32b4cfe95c2c73e6055
JZylber <zylberjulian@gmail.com>
2024-08-19 11:20:44 -0300

    feat(backend): basic serial port usage

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,16 @@
 import { onEvent, startServer } from "soquetic";
+import { SerialPort } from "serialport";
+
+const port = new SerialPort({
+  path: "/dev/tty-usbserial1",
+  baudRate: 57600,
+});
 
 onEvent("colorSeleccionado", (color) => {
-  console.log("Color seleccionado:", color);
+  const red = parseInt(color.slice(1, 3), 16);
+  const green = parseInt(color.slice(3, 5), 16);
+  const blue = parseInt(color.slice(5, 7), 16);
+  port.write(`${red},${green},${blue}\n`);
 });
 
 startServer();
